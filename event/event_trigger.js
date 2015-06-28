a3bf21428a161121f46bcf058d1cf1cc125b6e33
darionyaphet <darion.yaphet@gmail.com>
2015-06-28 23:01:39 +0800

    daily coding

--- event/event_trigger.js
+++ event/event_trigger.js
@@ -1,4 +1,14 @@
 var event   = require('events');
 var emitter = new event.EventEmitter();
 
+emitter.on('event_1' , function(arg){
+    console.log('Event_1 Called : '+' Arg : '+arg);
+});
+
+emitter.on('event_2' , function(arg0,arg1){
+    console.log('Event_2 Called : '+' Arg : '+arg0+' '+arg1);
+});
+
+emitter.emit('event_1','darion');
+emitter.emit('event_2','darion','yaphet');
 
